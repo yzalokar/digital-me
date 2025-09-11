b58523b560d71c3d162c2a8b5189575a9a1e7b89
yzalokar54 <yannick.zalokar@students.fhnw.ch>
2025-09-11 22:06:18 +0200

    feat: homepage component added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './home-page/home-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomePageComponent },
+];
